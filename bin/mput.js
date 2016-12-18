c67daf7875a140ac823cbd9f33d854de7ecdba5b
arunabh.trivedi@gmail.com <arunabh.trivedi@gmail.com>
2016-12-19 02:15:30 +0530

    changing mput to make it work, few defects

--- bin/mput.js
+++ bin/mput.js
@@ -5,7 +5,29 @@ var fs = require('fs');
 var sasurl = process.env.AE_BLOBSERVICE_SAS_URL
 var blobURL = sasurl.substring(0, sasurl.indexOf('?'));
 var blobCred = sasurl.substring(sasurl.indexOf('?'));
-var blobService = azure.createBlobService(null, null, blobURL, blobCred);
+
+var uploadFile = function(fileName, containerName, overwrite) {
+  var blobService = azure.createBlobService(null, null, blobURL, blobCred);
+  blobService.getBlobProperties(containerName, fileName, null, function(error, result, response){
+    if(error || overwrite)
+    {
+      var stats = fs.statSync(fileName);
+      var fileSizeInBytes = stats["size"];
+      var blobService_upload = azure.createBlobService(null, null, blobURL, blobCred);
+      blobService_upload.createBlockBlobFromStream(containerName, fileName, fs.createReadStream(fileName), fileSizeInBytes, function(error, result, response){
+        if(error){
+            console.log("Couldn't upload file %s", fileName);
+            console.error(error);
+        } else {
+            console.log('File %s uploaded successfully', fileName);
+        }
+      });
+    }
+    else {
+      console.log("Blob [%s] already exist in container [%s]", fileName, containerName);
+    }
+  });
+}
 
 exports.mput = function(containerName, filePattern, overwrite) {
     console.log(filePattern);
@@ -13,19 +35,12 @@ exports.mput = function(containerName, filePattern, overwrite) {
     for(var ix in files)
     {
       var fileName = files[ix];
-      blobService.getBlobProperties(containerName, fileName, null, function(error, result, response){
-        if(error || overwrite)
-        {
-          var put = require("./put.js");
-          put.put(containerName, fileName, fileName);
-        }
-        else {
-          console.log("Blob [%s] already exist in container [%s]", fileName, containerName);
-        }
-      });
-      //console.log("-----:" + files[ix])
-      /*
-      */
+      if(fs.lstatSync(fileName).isDirectory()) {
+        console.log("skipping directory: " + fileName);
+      }
+      else {
+        uploadFile(fileName, containerName, overwrite);
+      }
     }
   });
 }
